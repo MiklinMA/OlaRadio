dda99c233ac941e7cbe8a1e056d3937bcdd18d04
Mike Miklin <MiklinMA@gmail.com>
2022-11-12 07:06:00 +0500

    Initial Commit

---
+++ OlaRadioUI/OlaRadioUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlaRadioUIApp.swift
+//  OlaRadioUI
+//
+//  Created by Mike Miklin on 12.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct OlaRadioUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
